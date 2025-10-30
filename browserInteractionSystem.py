f2a98b8a569f2bc8c52fed2a6e3ede8ef42a0e27
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-10-29 22:12:54 -0700

    Update browserInteractionSystem.py

--- browserInteractionSystem.py
+++ browserInteractionSystem.py
@@ -2,38 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+#variables
+command = ""
+
 #functions
 def create_account():
-    print("filler")
-
-def login():
-    print("filler")
+    print("test1")
     
-def controller():
-    print("filler")
+def login():
+    print("test2")
 
-#start browser
+def command_guide():
+    print("test3")
+
+#call browser!
 driver = webdriver.Chrome()
-print("Chromium has opened!")
+print(f"{driver.name} activated!")
 
-#navigate to url
-driver.get("https://accounts.crypto.com/en-US/signup?utm_source=google")
-print(f"You have successfully entered {driver.title}")
+#move to!
+driver.get("https://accounts.crypto.com/en-US/signup?utm_campaign=Google+Search_MA_Web_CA_en_NA_CPA_Brand_20250401&utm_medium=cpc&utm_source=google")
+print(f"crypto.com has been opened!")
+time.sleep(4.0)
 
-#configure site settings
-time.sleep(5)
-customize_settings_button = driver.find_element(by=By.ID, value="onetrust-pc-btn-handler")
-customize_settings_button.click()
-print("Cookies options clicked!")
-time.sleep(5)
-reject_cookies = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[3]/div[1]/button[1]")
-reject_cookies.click()
-print("Reject non-essential clicked!")
-time.sleep(5)
-email_textbox_enter = driver.find_element(by=By.XPATH, value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div")
-email_textbox_enter.click()
-print("Email textbox clicked!")
-time.sleep(0.3)
-email_textbox = driver.find_element(by=By.NAME, value="mantine-dzbj6lbvf")
-email_textbox.send_keys("maxrodsto@outlook.com")
-print("Email entered!")
+#cookie configuration!
+customize_settings = driver.find_element(by=By.ID , value="onetrust-pc-btn-handler")
+print("Element found!")
+customize_settings.click()
+print("Have clicked settings!")
+time.sleep(3.0)
+reject_non_essential_cookies = driver.find_element(by=By.XPATH , value="/html/body/div[3]/div[3]/div/div[3]/div[1]/button[1]")
+print("Element found!")
+reject_non_essential_cookies.click()
+print("Have clicked settings!")
+time.sleep(3.0)
+
+#click email fill in!
+click_email_box = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div")
+print("Email box selected!")
+time.sleep(3.0)
+click_email_box.click()
+print("Email box clicked!")
+time.sleep(3.0)
+input_email_box = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/input[1]")
+input_email_box.send_keys("Example@gmail.com")
+print("Email has been entered!")
+time.sleep(3.0)
+submit_email = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[6]/button[1]")
+submit_email.click()
+
+#capacha automation
