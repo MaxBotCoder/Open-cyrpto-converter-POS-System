25ac65f32550a6538f29a7c355833d29c13d739b
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-10-30 16:26:12 -0700

    Update browserInteractionSystem.py

--- browserInteractionSystem.py
+++ browserInteractionSystem.py
@@ -3,51 +3,41 @@ from selenium.webdriver.common.by import By
 import time
 
 #variables
-command = ""
+url_crypto_dot_com = "https://accounts.crypto.com/en-US/signup?utm_campaign=Google+Search_MA_Web_CA_en_NA_CPA_Brand_20250401&utm_medium=cpc&utm_source=google" 
+url_buser_capacha_solver = "https://chromewebstore.google.com/detail/buster-captcha-solver-for/mpbjkejclgfgadiemmefgebjfooflfhl?hl=en-GB&utm_source=ext_sidebar"
+driver = webdriver.Chrome()
 
 #functions
-def create_account():
-    print("test1")
+def command_gateway():
+    print("filler")
     
-def login():
-    print("test2")
+def load_extensions():
+    driver.get(url_buser_capacha_solver)
+    print("Extension page entered!")
+    time.sleep(5.0)
+    add_to_chrome = driver.find_element(by=By.XPATH , value="/html/body/c-wiz/div/div/main/div/section[1]/section/div/div[1]/div[2]/div/button")
+    add_to_chrome.click()
+ 
+def create_account_general(email):
+    driver.get(url_crypto_dot_com)
+    print("Erypto.com! entered")
+    time.sleep(5.0)
+    reject_non_essential_cookies = driver.find_element(by=By.ID , value="onetrust-reject-all-handler")
+    reject_non_essential_cookies.click()
+    print("Rejected non essential cookies!")
+    time.sleep(5.0)
+    input_email = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/input")
+    input_email.send_keys(email)
+    print(f"{email} has been entered!")
+    time.sleep(5.0)
+    create_account_button = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[6]/button[1]/span")
+    create_account_button.click()
+    print("button clicked!")
+        
+def login_general():
+    print("filler")
+    
+#call functions
 
-def command_guide():
-    print("test3")
-
-#call browser!
-driver = webdriver.Chrome()
-print(f"{driver.name} activated!")
-
-#move to!
-driver.get("https://accounts.crypto.com/en-US/signup?utm_campaign=Google+Search_MA_Web_CA_en_NA_CPA_Brand_20250401&utm_medium=cpc&utm_source=google")
-print(f"crypto.com has been opened!")
-time.sleep(4.0)
-
-#cookie configuration!
-customize_settings = driver.find_element(by=By.ID , value="onetrust-pc-btn-handler")
-print("Element found!")
-customize_settings.click()
-print("Have clicked settings!")
-time.sleep(3.0)
-reject_non_essential_cookies = driver.find_element(by=By.XPATH , value="/html/body/div[3]/div[3]/div/div[3]/div[1]/button[1]")
-print("Element found!")
-reject_non_essential_cookies.click()
-print("Have clicked settings!")
-time.sleep(3.0)
-
-#click email fill in!
-click_email_box = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div")
-print("Email box selected!")
-time.sleep(3.0)
-click_email_box.click()
-print("Email box clicked!")
-time.sleep(3.0)
-input_email_box = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[3]/div/input[1]")
-input_email_box.send_keys("Example@gmail.com")
-print("Email has been entered!")
-time.sleep(3.0)
-submit_email = driver.find_element(by=By.XPATH , value="/html/body/div[2]/main/div[2]/div/div/div[1]/div/form/div[6]/button[1]")
-submit_email.click()
-
-#capacha automation
+load_extensions()
+create_account_general("maxrodsto@outlook.com")
